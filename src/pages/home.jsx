2e2acc8c2e3306cf6204ee14852092c55b1345c9
baucucu <alexandru.raduca@gmail.com>
2021-09-27 16:45:36 +0300

    icons update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,64 +12,72 @@ const HomePage = () => {
   const tabFontSize = '9px';
   
   return (
-  <Page>
+  <Page style={{backgroundColor: ''}}>
     <Toolbar tabbar labels position="top">
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-1"
       tabLinkActive
       // text="Brief"
-      iconIos="f7:envelope_fill"
-      iconAurora="f7:envelope_fill"
-      iconMd="material:email"
+      iconF7="textbox"
+      // iconIos="f7:envelope_fill"
+      // iconAurora="f7:envelope_fill"
+      // iconMd="material:email"
       iconSize="medium"
     >Brief</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-2"
-      iconIos="f7:calendar_fill"
-      iconAurora="f7:calendar_fill"
-      iconMd="material:today"
+      iconF7="doc_plaintext"
+      // iconIos="f7:calendar_fill"
+      // iconAurora="f7:calendar_fill"
+      // iconMd="material:today"
       iconSize="medium"
     >Manuscript</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-3"
-      iconIos="f7:calendar_fill"
-      iconAurora="f7:calendar_fill"
-      iconMd="material:today"
+      iconF7="cod_plaintext"
+      iconF7="music_mic"
+      // iconIos="f7:calendar_fill"
+      // iconAurora="f7:calendar_fill"
+      // iconMd="material:today"
       iconSize="medium"
     >Voiceover</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-4"
-      iconIos="f7:cloud_upload_fill"
-      iconAurora="f7:cloud_upload_fill"
-      iconMd="material:file_upload"
+      iconF7="photo"
+      // iconIos="f7:cloud_upload_fill"
+      // iconAurora="f7:cloud_upload_fill"
+      // iconMd="material:file_upload"
       iconSize="medium"
     >Storyboard</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-5"
-      iconIos="f7:envelope_fill"
-      iconAurora="f7:envelope_fill"
-      iconMd="material:email"
+      iconF7="doc_richtext"
+      // iconIos="f7:envelope_fill"
+      // iconAurora="f7:envelope_fill"
+      // iconMd="material:email"
       iconSize="medium"
     >Illustrations</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-6"
-      iconIos="f7:calendar_fill"
-      iconAurora="f7:calendar_fill"
-      iconMd="material:today"
+      iconF7="play_rectangle"
+      // iconIos="f7:calendar_fill"
+      // iconAurora="f7:calendar_fill"
+      // iconMd="material:today"
       iconSize="medium"
     >Animation</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-7"
-      iconIos="f7:cloud_upload_fill"
-      iconAurora="f7:cloud_upload_fill"
-      iconMd="material:file_upload"
+      iconF7="rocket"
+      // iconIos="f7:cloud_upload_fill"
+      // iconAurora="f7:cloud_upload_fill"
+      // iconMd="material:file_upload"
       iconSize="medium"
     >Delivery</Link>
   </Toolbar>
