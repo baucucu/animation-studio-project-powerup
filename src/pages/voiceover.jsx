51b93fd17bd9c8fe01b30d3e671a941b2dad1bb3
baucucu <alexandru.raduca@gmail.com>
2021-09-27 20:18:24 +0300

    update

--- src/pages/voiceover.jsx
+++ src/pages/voiceover.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import { Page, Navbar, Block, BlockTitle } from 'framework7-react';
+import React, {useEffect,useState} from 'react';
+import { Page, Navbar, Block, BlockTitle, Button, Icon } from 'framework7-react';
 
-const Voiceover = () => (
-  <>
-    <BlockTitle>Voiceover</BlockTitle>
-    <Block strong>
-      <p>Fugiat perspiciatis excepturi, soluta quod non ullam deleniti. Nobis sint nemo consequuntur, fugiat. Eius perferendis animi autem incidunt vel quod tenetur nostrum, voluptate omnis quasi quidem illum consequuntur, a, quisquam.</p>
-      <p>Laudantium neque magnam vitae nemo quam commodi, in cum dolore obcaecati laborum, excepturi harum, optio qui, consequuntur? Obcaecati dolor sequi nesciunt culpa quia perspiciatis, reiciendis ex debitis, ut tenetur alias.</p>
-    </Block>
-    <Block strong>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni molestiae laudantium dignissimos est nobis delectus nemo ea alias voluptatum architecto, amet similique, saepe iste consectetur in repellat ut minus quibusdam!</p>
-      <p>Molestias et distinctio porro nesciunt ratione similique, magni doloribus, rerum nobis, aliquam quae reiciendis quasi modi. Nam a recusandae, fugiat in ea voluptates fuga eius, velit corrupti reprehenderit dignissimos consequatur!</p>
-      <p>Blanditiis, cumque quo adipisci. Molestiae, dolores dolorum quos doloremque ipsa ullam eligendi commodi deserunt doloribus inventore magni? Ea mollitia veniam nostrum nihil, iusto doloribus a at! Ea molestiae ullam delectus!</p>
-    </Block>
-  </>
-);
+// var Promise = TrelloPowerUp.Promise;
+
+
+const Voiceover = () => {
+  var t = TrelloPowerUp.iframe();
+
+  const openModal = () => {
+    t.modal({
+      // the url to load for the iframe
+      url: 'https://f.io/C5JnqyCi',
+      fullscreen: true,
+      // optional function to be called if user closes modal (via `X` or escape, etc)
+      callback: () => console.log('Goodbye.'),
+      // optional title for header chrome
+      title: 'Voiceover',
+    })
+  }
+  return (
+    <>
+      <Block>
+        <Button fill raised color="purple" onClick={openModal}>
+          <Icon f7="doc_plaintext" style={{marginRight:8}} slot="icon"></Icon>
+          Open voiceover
+        </Button>
+      </Block>
+    </>
+  );
+}
 
 export default Voiceover;
