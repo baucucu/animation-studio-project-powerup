ed4b2aeb48c8faba51b5e8d4cb0eae9949c2f4f6
baucucu <alexandru.raduca@gmail.com>
2021-09-27 17:32:31 +0300

    brief ui update

--- src/pages/brief.jsx
+++ src/pages/brief.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import { Page, Navbar, Block, BlockTitle } from 'framework7-react';
+import React, {useEffect,useState} from 'react';
+import { Page, Navbar, Block, BlockTitle, Button } from 'framework7-react';
 
-const Brief = () => (
-  <>
-    <BlockTitle>Brief</BlockTitle>
-    <Block>
-      <iframe width="100%" height="500px"  src="https://studioflow.typeform.com/brief" title="Brief"></iframe>
-    </Block>
-  </>
-);
+// var Promise = TrelloPowerUp.Promise;
+
+
+const Brief = () => {
+  var t = TrelloPowerUp.iframe();
+
+  console.log("t: ", t)
+
+  const openModal = (t) => {
+    t.modal({
+      // the url to load for the iframe
+      url: 'https://studioflow.typeform.com/brief',
+      fullscreen: true,
+      // optional function to be called if user closes modal (via `X` or escape, etc)
+      callback: () => console.log('Goodbye.'),
+      // optional title for header chrome
+      title: 'Brief',
+    })
+  }
+
+
+  return (
+    <>
+      <BlockTitle>Brief</BlockTitle>
+      <Button onClick={openModal}
+      >
+        Open modal
+    </Button>
+      <Block>
+        <iframe width="100%" height="500px"  src="https://studioflow.typeform.com/brief" title="Brief"></iframe>
+      </Block>
+    </>
+  );
+}
 
 export default Brief;
