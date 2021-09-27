b97aee6720a2b3044e2990771b7da9d3db777994
baucucu <alexandru.raduca@gmail.com>
2021-09-27 18:36:30 +0300

    brief update

--- src/pages/brief.jsx
+++ src/pages/brief.jsx
@@ -4,8 +4,7 @@ import { Page, Navbar, Block, BlockTitle, Button, Icon } from 'framework7-react'
 // var Promise = TrelloPowerUp.Promise;
 
 
-const Brief = (props) => {
-  const {isCompleted = true} = props
+const Brief = ({isCompleted = true}) => {
   var t = TrelloPowerUp.iframe();
 
   const openModal = () => {
@@ -19,15 +18,23 @@ const Brief = (props) => {
       title: 'Brief',
     })
   }
-
-
   return (
-    <Block>
-      <Button fill raised color="purple" onClick={openModal}>
-        <Icon f7="textbox" style={{marginRight:8}} slot="icon"></Icon>
-        Open brief form
-      </Button>
-    </Block>
+    <>
+      {!isCompleted && <Block>
+        <p>In order to start your project, we need to collect some information about your company, product, and your expectations. This will lay the ground for the whole project and will be shared with all creators working on the project.</p>
+        <p>The questionnaire contains about 40 questions and takes roughly 30 minutes to respond to. Make sure you have gathered all involved parties on your side when answering the questions in the brief. The more detailed the better it is!
+Click the link below to start the questionnaire.</p>
+        <Button fill raised color="purple" onClick={openModal}>
+          <Icon f7="textbox" style={{marginRight:8}} slot="icon"></Icon>
+          Open brief questionnaire
+        </Button>
+      </Block>}
+      {isCompleted && <Block>
+        <p>You have already competed the brief questionnaire</p>
+        <p>Answers</p>
+        <p>...</p>
+      </Block>}
+    </>
   );
 }
 
