cbbb60fb830d693e55fc8dc4ad0134b6653cb432
baucucu <alexandru.raduca@gmail.com>
2021-09-27 16:55:43 +0300

    tabs animation

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,20 +18,13 @@ const HomePage = () => {
       style={{fontSize:tabFontSize}}
       tabLink="#tab-1"
       tabLinkActive
-      // text="Brief"
       iconF7="textbox"
-      // iconIos="f7:envelope_fill"
-      // iconAurora="f7:envelope_fill"
-      // iconMd="material:email"
       iconSize="medium"
     >Brief</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-2"
       iconF7="doc_plaintext"
-      // iconIos="f7:calendar_fill"
-      // iconAurora="f7:calendar_fill"
-      // iconMd="material:today"
       iconSize="medium"
     >Manuscript</Link>
     <Link
@@ -39,50 +32,35 @@ const HomePage = () => {
       tabLink="#tab-3"
       iconF7="cod_plaintext"
       iconF7="music_mic"
-      // iconIos="f7:calendar_fill"
-      // iconAurora="f7:calendar_fill"
-      // iconMd="material:today"
       iconSize="medium"
     >Voiceover</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-4"
       iconF7="photo"
-      // iconIos="f7:cloud_upload_fill"
-      // iconAurora="f7:cloud_upload_fill"
-      // iconMd="material:file_upload"
       iconSize="medium"
     >Storyboard</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-5"
       iconF7="doc_richtext"
-      // iconIos="f7:envelope_fill"
-      // iconAurora="f7:envelope_fill"
-      // iconMd="material:email"
       iconSize="medium"
     >Illustrations</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-6"
       iconF7="play_rectangle"
-      // iconIos="f7:calendar_fill"
-      // iconAurora="f7:calendar_fill"
-      // iconMd="material:today"
       iconSize="medium"
     >Animation</Link>
     <Link
       style={{fontSize:tabFontSize}}
       tabLink="#tab-7"
       iconF7="rocket"
-      // iconIos="f7:cloud_upload_fill"
-      // iconAurora="f7:cloud_upload_fill"
-      // iconMd="material:file_upload"
       iconSize="medium"
     >Delivery</Link>
   </Toolbar>
 
-  <Tabs>
+  <Tabs animated>
     <Tab id="tab-1" className="page-content" tabActive>
       <Brief/>
     </Tab>
