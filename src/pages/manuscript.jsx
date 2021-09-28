3392a8062e08a03b41dfd53e3b48283fce13fd4d
baucucu <alexandru.raduca@gmail.com>
2021-09-28 13:21:54 +0300

    update

--- src/pages/manuscript.jsx
+++ src/pages/manuscript.jsx
@@ -4,7 +4,10 @@ import { Page, Navbar, Block, BlockTitle, Button, Icon } from 'framework7-react'
 // var Promise = TrelloPowerUp.Promise;
 
 
-const Manuscript = () => {
+const Manuscript = (props) => {
+  
+  const {state="active"} = props
+
   var t = TrelloPowerUp.iframe();
 
   const openModal = () => {
@@ -20,12 +23,19 @@ const Manuscript = () => {
   }
   return (
     <>
-      <Block>
+      {state === "locked" && <Block style={{paddingLeft:0}}>
+        <img width="30%" src='../assets/locked-manuscript.gif'></img>
+      </Block>}
+      {state !== "locked" && <Block>
+        <p>The script is the absolute core of the whole video project. Without a good script, it does not matter how lovely illustrations or good narrator voice we have.<p/>
+        <p>Make sure you are delighted with the manuscript before accepting it. Once approved, we will not be able to make any changes while working on the other steps.</p>
+        <p>Click the button below to review the manuscript.</p>
+        </p>
         <Button fill raised color="purple" onClick={openModal}>
           <Icon f7="doc_plaintext" style={{marginRight:8}} slot="icon" size="small"></Icon>
           Open manuscript
         </Button>
-      </Block>
+      </Block>}
     </>
   );
 }
